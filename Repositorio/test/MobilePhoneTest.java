6811989159219f2a10f45c465f013bedd3ae4d24
emmanuel <emmanuel.portugal.91@gmail.com>
2016-02-18 22:20:44 -0300

    codificando test

---
+++ Repositorio/test/MobilePhoneTest.java
@@ -0,0 +1,18 @@
+
+import repositorio.MobilePhone;
+import repositorio.SocialSharer;
+import org.junit.Test;
+import static org.mockito.Mockito.*;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Emmanuel
+ */
+public class MobilePhoneTest {
+    
+    public MobilePhoneTest() {
+    }
+    
+    
+}
