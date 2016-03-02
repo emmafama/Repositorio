1343bd8a0ef4681337fcc6a3bdcb6afab341e6ce
emmanuel <emmanuel.portugal.91@gmail.com>
2016-03-02 18:13:54 -0300

    test

---
+++ test/repositorio/FacebookSharerTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repositorio;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Emmanuel
+ */
+public class FacebookSharerTest {
+    
+    public FacebookSharerTest() {
+    }
+
+    @Test
+    public void testSomeMethod() {
+    }
+    
+}
